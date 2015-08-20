defc3997b25553066fb09adb9aeb68bd57ded0d1
Thomas Müller <mr.thomas.mueller@gmail.com>
2015-08-20 20:01:54 +0200

    no message

--- record_sim_flights_pwm/record_sim_flights_pwm/record_sim_flights_pwm.ino
+++ record_sim_flights_pwm/record_sim_flights_pwm/record_sim_flights_pwm.ino
@@ -1,62 +1,83 @@
-#include <SD.h>
-#include <SPI.h>
+#include "avr/interrupt.h"
+
+unsigned int rc[6];
+float timer_period;
+unsigned int T, T_reset, T_prev, T_offset, T_min, T_max;
+unsigned short T_pointer=0;
+unsigned int tics_before_reset;
  
-File myFile;
-
-unsigned long duration = 0;
-unsigned long start = 0;
-
-unsigned int RECORD_PITCH_PIN = 6;
-unsigned int RECORD_RUDDER_PIN = 7;
-unsigned int RECORD_NICK_PIN = 8;
-unsigned int RECORD_ROLL_PIN = 9;
-
-
-void setup() {
-   Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+void setup()
+{ 
+  Serial.begin(115200);
+  
+  for (int i=6; i<=9; i++) {
+    pinMode(i,INPUT);
   }
-   Serial.print("Initializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin 
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
-  // or the SD library functions will not work. 
-   pinMode(10, OUTPUT);
-   
-  if (!SD.begin(4)) {
-    Serial.println("initialization failed!");
-    return;
+  
+  // Timer1 setup
+  TCCR1A = 0b00000000;          
+  TCCR1B = 0b00000011;        // Start timer1 with prescaler 8
+  timer_period = 0.000004;    // One tick takes 4 us
+  PORTB|=0b00111111;          // Turn on pullup resistors
+  PCMSK0=0b00111111;          // Pin change interrupt 0 bit mask: we interrupt on change on pins PCINT0-5 (arduino nano pins 8 - 13)
+  PCICR=0b00000001;           // Enable pin change interrupt 0
+  sei();                      // Global interrupt enable  
+  TCNT1 = 0;                  // Reset timer1
+
+  T_min = (unsigned int) (0.0005 / timer_period); // shorter than 0.5ms pwm signal is ignored
+  T_max = (unsigned int) (0.003 / timer_period); // longer than 3ms pwm signal indicates frame end
+  rc[0]=375;                  // Init rc with sane values
+  rc[1]=375;
+  rc[2]=375;
+  rc[3]=375;
+  rc[4]=375;
+  rc[5]=375;
+
+  tics_before_reset = (unsigned int) (0.10/timer_period); // 0.1 sec control loop (25000 tics)
+  
+  Serial.println("Read RC receiver test");
+  delay(1000);
+}
+
+void loop() //Main Loop
+{
+  while (TCNT1 < tics_before_reset) ; // Wait until start of new frame
+ 
+  cli(); // No interrupts allowed when modifying timer1
+  T_reset = TCNT1;
+  TCNT1 = 0;
+  T_offset = T_reset - T_prev;
+  T_prev=0;
+  sei(); // Reactivate interrupts. 
+  
+  // Other stuff goes here: Calculations, sensor readings, generating PWM on other pins, ...
+  printdata();
+}
+
+
+ISR(PCINT0_vect) {
+  T = TCNT1 - T_prev + T_offset;
+  if (T > T_min) {
+    T_prev = TCNT1;
+    T_offset=0;
+    if (T > T_max) {
+      T_pointer = 0;
+    } 
+    else {
+      rc[T_pointer] = T;
+      T_pointer++;
+      if (T_pointer > 5) T_pointer--; // overflow protection (glitches can happen...)
+    }
   }
-  Serial.println("initialization done.");
-  // re-open the file for reading:
-   
-  start = millis();
 }
+ 
 
-void loop() {
-    
-      writeToFile( RECORD_PITCH_PIN, "pitch.txt");
-      writeToFile( RECORD_RUDDER_PIN, "rudder.txt");
-      writeToFile( RECORD_NICK_PIN, "nick.txt");
-      writeToFile( RECORD_ROLL_PIN, "roll.txt");
-   
-    
+void printdata(void) {
+  for (int i=0; i<6; i++) {
+    String test = "test:";
+    test += rc[i];
+    Serial.println(test);
+  }
+
 }
-
-void writeToFile(unsigned int pin, const char *filename) {
-       duration = pulseIn(pin, HIGH);
-        File dataFile = SD.open(filename, FILE_WRITE);
-
-        // if the file is available, write to it:
-        if (dataFile) {
-           dataFile.println(duration);
-           dataFile.close();
-        } else {
-           Serial.println("error opening datalog.txt");
-        }
-}
-
-
-
 
