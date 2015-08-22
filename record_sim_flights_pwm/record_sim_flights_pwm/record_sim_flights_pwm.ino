e39539708b5fc6fbbe42c0d0cf7692dabf7d60c2
Thomas Müller <mr.thomas.mueller@gmail.com>
2015-08-22 13:36:08 +0200

    no message

--- record_sim_flights_pwm/record_sim_flights_pwm/record_sim_flights_pwm.ino
+++ record_sim_flights_pwm/record_sim_flights_pwm/record_sim_flights_pwm.ino
@@ -1,3 +1,9 @@
+#include <SPI.h>
+#include <SD.h>
+
+
+const int chipSelect = 4;
+
 volatile uint8_t prev; // remembers state of input bits from previous interrupt
 volatile uint32_t risingEdge[6]; // time of last rising edge for each channel
 volatile uint32_t uSec[6]; // the latest measured pulse width for each channel
@@ -44,14 +50,28 @@ void setup() {
   Serial.begin(9600);
     pinMode(2, INPUT);
     pinMode(3, INPUT);
-      pinMode(4, INPUT);
+   
     pinMode(5, INPUT);
     pinMode(6, INPUT);
     pinMode(7, INPUT);
   
 
-  PCMSK2 |= 0xFC; // set the mask to allow those 6 pins to generate interrupts
+  PCMSK2 |= 0xE4; // set the mask to allow those 6 pins to generate interrupts
   PCICR |= 0x04;  // enable interupt for port D
+  
+  Serial.print("\nInitializing SD card...");
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output
+  // or the SD library functions will not work.
+  pinMode(10, OUTPUT);     // change this to 53 on a mega
+  digitalWrite(10, HIGH);
+  
+ if (!SD.begin(chipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
 
   start = millis();
 }
@@ -67,21 +87,30 @@ void loop() {
   time = millis() - start;
   if (IR_ACTIVE) {
     if ( ((time % 40) == 0) | (time == 0) ) {
-      Serial.flush();  
-      Serial.print(counter);
-      Serial.print("\t");
-      Serial.print(mapTo100(uSec[0]));
-      Serial.print("\t");
-        Serial.print(mapTo100(uSec[1]));
-      Serial.print("\t");
-      Serial.print(mapTo100(uSec[2]));
-      Serial.print("\t");
-       Serial.print(mapTo100(uSec[3]));
-      Serial.print("\t");
-       Serial.print(mapTo100(uSec[4]));
-    
+      String stickmoves = "";
+      stickmoves += counter;
+      stickmoves += "\t";
+      stickmoves += mapTo100(uSec[0]);
+      stickmoves += "\t";
+      stickmoves += mapTo100(uSec[1]);       
+      stickmoves += "\t";
+      stickmoves += mapTo100(uSec[2]);    
+      stickmoves += "\t";
+      stickmoves += mapTo100(uSec[3]);    
+      stickmoves += "\t";
+      stickmoves += mapTo100(uSec[4]);    
+        
+      File dataFile = SD.open("stickmoves.txt", FILE_WRITE);
   
-      Serial.println();
+      // if the file is available, write to it:
+      if (dataFile) {
+        dataFile.println(stickmoves);
+        dataFile.close();
+      } else {
+        Serial.println("error opening datalog.txt");
+      }
+      Serial.flush();  
+      Serial.println(stickmoves);
       counter++;
     }
   }
