0e8f90959664d0272cfcd0395702b7ff601ca4d2
Thomas Müller <mr.thomas.mueller@gmail.com>
2017-02-02 02:18:28 +0100

    removed asf dependency from ppm_capture.c

--- babyphone/babyphone/src/includes/registers.h
+++ babyphone/babyphone/src/includes/registers.h
@@ -10,42 +10,7 @@
 #define REGISTERS_H_
 
 #include "stdint.h"
-//	ADC registers
-#define REG_ADC_CDR6	(*(volatile  uint32_t*)0x400C0068U) // ADC Channel Data Register
-#define REG_ADC_CDR7	(*(volatile  uint32_t*)0x400C006CU) // ADC Channel Data Register
 
-// NVIC registers
-#define ISER0  (*(volatile uint32_t*)0xE000E100U)
-#define ICER0  (*(volatile uint32_t*)0xE000E180U)
-#define ICPR0  (*(volatile uint32_t*)0xE000E280U)
-#define ISER1  (*(volatile uint32_t*)0xE000E104U)
-#define ICER1  (*(volatile uint32_t*)0xE000E184U)
-#define ICPR1  (*(volatile uint32_t*)0xE000E284U)
-
-
-// PIO registers
-#define REG_PIOC_PDR	(*(volatile uint32_t*)0x400E1204U)
-#define REG_PIOC_SODR   (*(volatile uint32_t*)0x400E1230U) /**< \brief (PIOC) Set Output Data Register */
-#define REG_PIOC_CODR   (*(volatile uint32_t*)0x400E1234U) /**< \brief (PIOC) Clear Output Data Register */
-
-// ADC registers
-#define REG_ADC_CR		(*(volatile uint32_t*)0x400C0000U)
-#define REG_ADC_MR		(*(volatile uint32_t*)0x400C0004U)
-#define REG_ADC_CHER	(*(volatile uint32_t*)0x400C0010U)
-#define REG_ADC_CGR		(*(volatile uint32_t*)0x400C0048U)
-#define REG_ADC_IER		(*(volatile uint32_t*)0x400C0024U)
-
-// PMC registers
-#define PMC_PCER0		(*(volatile uint32_t*)0x400E0610U)
-#define REG_PMC_PCER1	(*(volatile uint32_t*)0x400E0700U)
-
-
-// TC0 channel 1 registers (PID controller update)
-#define TC0_CHANNEL1_CCR  (*(volatile uint32_t*)0x40080040U)
-#define TC0_CHANNEL1_IER  (*(volatile uint32_t*)0x40080064U)
-#define TC0_CHANNEL1_CMR  (*(volatile uint32_t*)0x40080044U)
-#define TC0_CHANNEL1_RC   (*(volatile uint32_t*)0x4008005CU)
-#define TC0_CHANNEL1_SR   (*(volatile uint32_t*)0x40080060U)
 
 // PWM registers
 #define REG_PWM_DIS      (*(volatile uint32_t*)0x40094008U) /**< \brief (PWM) PWM Disable Register */
@@ -88,7 +53,52 @@
 #define REG_PIOC_ABSR    (*(volatile uint32_t*)0x400E1270U) /**< \brief (PIOC) Peripheral AB Select Register */
 #define REG_PWM_ENA      (*(volatile uint32_t*)0x40094004U) /**< \brief (PWM) PWM Enable Register */
 #define REG_PWM_SCUC     (*(volatile uint32_t*)0x40094028U) /**< \brief (PWM) PWM Sync Channels Update Control Register */
-   
+
+
+//	ADC registers
+#define REG_ADC_CDR6	(*(volatile  uint32_t*)0x400C0068U) // ADC Channel Data Register
+#define REG_ADC_CDR7	(*(volatile  uint32_t*)0x400C006CU) // ADC Channel Data Register
+
+// NVIC registers
+#define ISER0  (*(volatile uint32_t*)0xE000E100U)
+#define ICER0  (*(volatile uint32_t*)0xE000E180U)
+#define ICPR0  (*(volatile uint32_t*)0xE000E280U)
+#define ISER1  (*(volatile uint32_t*)0xE000E104U)
+#define ICER1  (*(volatile uint32_t*)0xE000E184U)
+#define ICPR1  (*(volatile uint32_t*)0xE000E284U)
+
+
+// PIO registers
+#define REG_PIOC_PDR	(*(volatile uint32_t*)0x400E1204U)
+#define REG_PIOC_SODR   (*(volatile uint32_t*)0x400E1230U) /**< \brief (PIOC) Set Output Data Register */
+#define REG_PIOC_CODR   (*(volatile uint32_t*)0x400E1234U) /**< \brief (PIOC) Clear Output Data Register */
+
+// ADC registers
+#define REG_ADC_CR		(*(volatile uint32_t*)0x400C0000U)
+#define REG_ADC_MR		(*(volatile uint32_t*)0x400C0004U)
+#define REG_ADC_CHER	(*(volatile uint32_t*)0x400C0010U)
+#define REG_ADC_CGR		(*(volatile uint32_t*)0x400C0048U)
+#define REG_ADC_IER		(*(volatile uint32_t*)0x400C0024U)
+
+// PMC registers
+#define PMC_PCER0		(*(volatile uint32_t*)0x400E0610U)
+#define REG_PMC_PCER1	(*(volatile uint32_t*)0x400E0700U)
+
+
+// TC0 channel 1 registers (PID controller update)
+#define TC0_CHANNEL1_CCR  (*(volatile uint32_t*)0x40080040U)
+#define TC0_CHANNEL1_IER  (*(volatile uint32_t*)0x40080064U)
+#define TC0_CHANNEL1_CMR  (*(volatile uint32_t*)0x40080044U)
+#define TC0_CHANNEL1_RC   (*(volatile uint32_t*)0x4008005CU)
+#define TC0_CHANNEL1_SR   (*(volatile uint32_t*)0x40080060U)
+
+// TC2 channel 0 registers (ppm capture)
+#define TC2_CHANNEL0_CCR  (*(volatile uint32_t*)0x40088000U)
+#define TC2_CHANNEL0_IER  (*(volatile uint32_t*)0x40088024U)
+#define TC2_CHANNEL0_CMR  (*(volatile uint32_t*)0x40088004U)
+#define TC2_CHANNEL0_SR   (*(volatile uint32_t*)0x40088020U)
+#define TC2_CHANNEL0_RB   (*(volatile uint32_t*)0x40088018U)
+
 // TC control bits
 #define TC_CMR_TCCLKS_TIMER_CLOCK1 (0x0u << 0)
 #define TC_CMR_WAVE (0x1u << 15)
@@ -97,8 +107,15 @@
 #define TC_CMR_CPCTRG (0x1u << 14)
 #define TC_SR_CPCS (0x1u << 4) /**< \brief (TC_SR) RC Compare Status */
 
+#define TC_CMR_TCCLKS_TIMER_CLOCK3 (0x2u << 0) /**< \brief (TC_CMR) Clock selected: TCLK3 */
+#define TC_CMR_LDRA_FALLING (0x2u << 16) /**< \brief (TC_CMR) Falling edge of TIOA */
+#define TC_CMR_LDRB_RISING (0x1u << 18) /**< \brief (TC_CMR) Rising edge of TIOA */
+#define TC_CMR_ABETRG (0x1u << 10) /**< \brief (TC_CMR) TIOA or TIOB External Trigger Selection */
+#define TC_CMR_ETRGEDG_RISING (0x1u << 8) /**< \brief (TC_CMR) Rising edge */
 
- 
- 
+// TC interrupt bits
+#define TC_IER_LDRBS (0x1u << 6) /**< \brief (TC_IER) RB Loading */
+#define TC_SR_LDRBS (0x1u << 6) /**< \brief (TC_SR) RB Loading Status */
+
 
 #endif /* REGISTERS_H_ */
--- babyphone/babyphone/src/modules/ppm_capture.c
+++ babyphone/babyphone/src/modules/ppm_capture.c
@@ -1,22 +1,7 @@
-
-#include <asf.h>
 #include <conf_board.h>
 #include <conf_clock.h>
 #include <includes/ppm_capture.h>
-
-#define TC_CAPTURE_TIMER_SELECTION TC_CMR_TCCLKS_TIMER_CLOCK3
-/** Use TC Peripheral 2 **/
-#define TC  TC2
-
-/** Configure TC2 channel 0 as capture input. **/
-#define TC_CHANNEL_CAPTURE 0
-#define ID_TC_CAPTURE      ID_TC6  //TC6 is channel 0 of TC2 module!!
-
-// #define PIN_TC_CAPTURE_MUX IOPORT_MODE_MUX_B // peripheral B // TODO warum ist das Anschalten des pins nicht erforderlich??
-
-/** Use TC2_Handler for TC capture interrupt**/
-#define TC_Handler  TC6_Handler
-#define TC_IRQn     TC6_IRQn
+#include "includes/registers.h"
 
 #define PPM_OFFSET 400
 
@@ -26,11 +11,11 @@ static uint32_t gs_ul_captured_rb;
 
 static int channel_id = 0;
 
-void TC_Handler(void)
+void TC6_Handler(void)
 {
-	if ((tc_get_status(TC, TC_CHANNEL_CAPTURE) & TC_SR_LDRBS) == TC_SR_LDRBS) {
+	if ((TC2_CHANNEL0_SR & TC_SR_LDRBS) == TC_SR_LDRBS) {
 		uint16_t micros = 0;
-		gs_ul_captured_rb = tc_read_rb(TC, TC_CHANNEL_CAPTURE);
+		gs_ul_captured_rb = TC2_CHANNEL0_RB;
 		micros = (gs_ul_captured_rb * 8) / 21; // clock is Master Clock with prescaler 32 at 84 MHz Master Clock
 		if (micros > 3000) {
 			// PPM sync pulse, recount channels starting with 0
@@ -60,23 +45,20 @@ void ppm_capture_initialize(void)
 		rc_channels[i].last_captured_ppm_value = 0;
 	}
 	
-	pmc_enable_periph_clk(ID_TC_CAPTURE);
-	tc_init(TC, TC_CHANNEL_CAPTURE,
-	TC_CAPTURE_TIMER_SELECTION /* Clock Selection */
-	| TC_CMR_LDRA_FALLING /* RA Loading: rising edge of TIOA */
-	| TC_CMR_LDRB_RISING /* RB Loading: falling edge of TIOA */
-	| TC_CMR_ABETRG /* External Trigger: TIOA */
-	| TC_CMR_ETRGEDG_RISING /* External Trigger Edge: Falling edge */
-	);
+	REG_PMC_PCER1 = REG_PMC_PCER1 | 0x00000002u;
 	
-	NVIC_DisableIRQ(TC_IRQn);
-	NVIC_ClearPendingIRQ(TC_IRQn);
-	NVIC_SetPriority(TC_IRQn, 5);
-	NVIC_EnableIRQ(TC_IRQn);
+	TC2_CHANNEL0_CCR = TC2_CHANNEL0_CCR | 0x00000002u;
+	TC2_CHANNEL0_CMR = TC_CMR_TCCLKS_TIMER_CLOCK3 | TC_CMR_LDRA_FALLING | TC_CMR_LDRB_RISING 	| TC_CMR_ABETRG | TC_CMR_ETRGEDG_RISING;
 	
-	tc_enable_interrupt(TC, TC_CHANNEL_CAPTURE, TC_IER_LDRBS);
-	tc_start(TC, TC_CHANNEL_CAPTURE);
+	ICER1 = ICER1 |  0x00000002u;
+	ICPR1 = ICPR1 |  0x00000002u;
 	
-	
+// 	NVIC_SetPriority(TC_IRQn, 5);
+	ISER1 = ISER1 | 0x00000002u;
+	// enable interrupt
+	TC2_CHANNEL0_IER = TC2_CHANNEL0_IER | TC_IER_LDRBS;
+	// start tc2 channel 0
+    TC2_CHANNEL0_CCR = TC2_CHANNEL0_CCR | 0x00000005u;
+
 }
 
